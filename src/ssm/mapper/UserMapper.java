090a3398e4edcf3fe88825ba1546157df3fd13ae
nishengwu <ni_shengwu@126.com>
2016-06-14 20:46:07 +0800

    Spring和MyBatis整合（mapper方法）

---
+++ src/ssm/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package ssm.mapper;
+
+
+import ssm.po.User;
+//mapper接口，相当于dao接口
+public interface UserMapper {
+
+	//根据id查询用户信息
+	public User findUserById(int id) throws Exception;
+
+}	
---
+++ test/ssm/mapper/UserMapperTest.java
@@ -0,0 +1,26 @@
+package ssm.mapper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ssm.po.User;
+
+public class UserMapperTest {
+
+private ApplicationContext applicationContext;
+	
+	@Before
+	public void setUp() throws Exception {
+		applicationContext = new ClassPathXmlApplicationContext("classpath:beans.xml");
+	}
+	
+	@Test
+	public void testFindUserById() throws Exception {
+		UserMapper userMapper = (UserMapper) applicationContext.getBean("userMapper");
+		User user = userMapper.findUserById(1);
+		System.out.println(user);
+	}
+
+}
